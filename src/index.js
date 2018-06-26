f6fc0b37c3ba8569c333c97c247cfe8d6c33aeea
Ben Robinson <ben@joinfoodchain.com>
2018-06-26 22:21:46 +0100

    Parse graphql result

--- src/index.js
+++ src/index.js
@@ -5,6 +5,41 @@ import { createHttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import gql from "graphql-tag"
 
+import parsePassage from "./parsePassage"
+
+type Sermon = {
+    id: string,
+    title: string,
+    passage: string,
+    seriesTitle: string,
+    seriesSubtitle: string,
+    preachedAt: string,
+    duration: number,
+    link: string,
+    imageUrl: string,
+    author: string,
+    event: string,
+}
+
+const parseSermon = (sermon: Object): ?Sermon => {
+    if (sermon.speakers.length === 0) {
+        return null
+    }
+    return {
+        id: sermon.id,
+        title: sermon.name,
+        passage: parsePassage("niv-long", sermon.passage),
+        seriesTitle: sermon.series.name,
+        seriesSubtitle: sermon.series.subtitle,
+        preachedAt: sermon.preachedAt,
+        duration: sermon.duration,
+        link: sermon.url,
+        imageUrl: sermon.series["image3x2Url"],
+        author: sermon.speakers[0].name,
+        event: sermon.event.name,
+    }
+}
+
 const main = async () => {
     const client = new ApolloClient({
         link: createHttpLink({
@@ -17,20 +52,28 @@ const main = async () => {
         query: gql`
             query {
                 allSermons(orderBy: preachedAt_DESC, first: 40) {
+                    id
                     name
                     series {
                         name
                         subtitle
+                        image3x2Url
                     }
                     preachedAt
                     speakers {
+                        name
+                    }
+                    passage
+                    duration
+                    url
+                    event {
                         name
                     }
                 }
             }
         `,
     })
-    console.log(result.data)
+    console.log(result.data.allSermons.map(s => parseSermon(s)))
 }
 
 main()
