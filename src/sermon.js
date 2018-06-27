e92e98fcd2f1b5f3f570365880066ccea4a1a413
Ben Robinson <ben@joinfoodchain.com>
2018-06-27 21:32:09 +0100

    Create rss feed

---
+++ src/sermon.js
@@ -0,0 +1,15 @@
+// @flow
+
+export type Sermon = {
+    id: string,
+    title: string,
+    passage: string,
+    seriesTitle: string,
+    seriesSubtitle: ?string,
+    preachedAt: string,
+    duration: number,
+    link: string,
+    imageUrl: ?string,
+    author: string,
+    event: string,
+}
